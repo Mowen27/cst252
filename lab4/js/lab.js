20b0f5f1acd452b0bca93b296637458a4e9555c9
Mowen27 <70286117+Mowen27@users.noreply.github.com>
2021-02-08 13:10:02 -0800

    updates

---
+++ lab4/js/lab.js
@@ -0,0 +1,8 @@
+//Rock, Paper, Sccissors Game
+//By Michael Owen
+//Feb, 2021
+
+// Display the three options
+  // Accept user input.
+  // Choose the computer’s move.
+// Evaluate if the user won, tied, or lost.
