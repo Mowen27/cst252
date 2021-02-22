e22e2ca02b59e55206a86ce54c5de09cb5ae2e9f
Mowen27 <70286117+Mowen27@users.noreply.github.com>
2021-02-22 12:25:08 -0800

    lab 7

---
+++ lab7/js/lab.js
@@ -0,0 +1,7 @@
+// lab7.js - Functions
+// Author: Michael Owen <miowen@csumb.edu>
+// Created: 2-15-21
+// License: Public Domain
+// Source borrowed from Wes Modes <wmodes@ucsc.edu>
+
+// Define Variables
