58bcabf1039d8f484a4a4130241b93c06efa34b6
Mowen27 <70286117+Mowen27@users.noreply.github.com>
2021-03-24 12:27:44 -0700

    new

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,18 +1,31 @@
-// Lab 14: Ojects, Classes, and Constructors
+// Lab 15: Object-Oriented Design
 // Author: Michael Owen <miowen@csumb.edu>
-// Created: 3-23-21
+// Created: 3-24-21
 // License: Public Domain
 
-//function Vehicle(make, model, year, color, extras) {
-  //this.make = make;
-  //this.model = model;
-  //this.year = year;
-  //this.color = color;
-  //this.extras = extras;
-  //this.info = function() {
-    //return this.color + " " + this.year + " " + this.make + " " + this.model + "with" + this.extras;
-  //}
-//}
+// Hang Man OOP
+
+// controls the game play
+// class HangMan
+  // initialize
+    // set up hang display
+    //set up the players
+    //assign the guessing player
+
+  // play
+    // loop 6 times for each wrong guess
+      // save answer from opposing player
+      // display correct letters below hang display
+      // display wrong letter at top of the screen
+      // player loses if they fail guessing 6 times
+      // swap out players
+
+  // check_game_over
+    // check_victory
+
+  // check_victory
+    // If the player filled out the oppising player's word
+      // display victory message "You got it!"
 
 class Vehicle {
   constructor(make, model, year, color, extras) {
