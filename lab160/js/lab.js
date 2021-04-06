5ac6eb6f08796536adbd33b163c755ee3ed801a5
Mowen27 <70286117+Mowen27@users.noreply.github.com>
2021-04-06 11:40:48 -0700

    new

---
+++ lab160/js/lab.js
@@ -0,0 +1,13 @@
+// Lab 16 Libraries & Intro to jQuery
+// Author: Michael Owen <miowen@csumb.edu>
+// Created: 4-6-21
+// License: Public Domain
+
+<form action="">
+  Name: <input type="text" name="user"><br>
+  Password: <input type="password" name="password"><br>
+  <button type="button">Useless Button</button>
+  <input type="button" value="Another useless button"><br>
+  <input type="reset" value="Reset">
+  <input type="submit" value="Submit"><br>
+</form>
